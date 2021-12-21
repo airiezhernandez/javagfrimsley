765a0538a9a9d628544907087a31f3cc1cd58d00
AiriezHernandez <77013673+airiezhernandez@users.noreply.github.com>
2021-12-21 14:02:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//alert('Hello')
+//alert('World')
+
+
+//alert(3 +
+//1
+//+ 2);
+
+//alert("Hello");
+
+//[1, 2].forEach(alert);
+
+//alert("Hello");[1, 2].forEach(alert);
+
+// This comment occupies a line of its own
+//alert('Hello');
+
+//alert('World'); // This comment follows the statement
+
+/* An example with two messages.
+This is a multiline comment.
+*/
+// alert('Hello');
+// alert('World');
+
+/* Commenting out the code
+alert('Hello');
+alert('World'); */
+
+// "use strict";
+
+// this code works the modern way
+// ...
+
+//'use strict'; <Shift+Enter for a newline>
+//  ...your code
+//<Enter to run>
+
+//(function() {
+//  'use strict';
+//
+//  alert('Hello World')
+//})()
+
+//let message;
+//message = 'Hello!';
+
+//alert(message); // shows the variable content
+
+
